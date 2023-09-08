da9c4b64d3724e4db97888c34f3fde29c9951771
asdf <tayeb1293@yahoo.com>
2023-09-08 16:48:06 +0200

    config routing

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const express = require('express'); 
 const app = express();
+// create blog router
+const blogRoute = require('./routes/blogRoute');
 
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// set the static files 
+app.use(express.static('public'));
+
+// Home Page
 app.get('/', (req, res) => {
-    res.send('test');
+    res.redirect('/blogs')
+});
+
+// About Page
+app.get('/about', (req, res) => {
+    res.render('about', { title:'About' });
+});
+
+// blog routes
+app.use('/blogs', blogRoute);
+
+// 404 page
+app.use((req, res) => {
+    res.status(404).render('./views/404.ejs'), { title:'404'}
 })
 
-app.get('/blogs', () => {
-    res.send("test Blogs");
-})
 
 // listening to requests
 app.listen(5000, () => {
---
+++ routes/blogRoute.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+// home page 
+router.get('/', (req, res) => {
+    console.log(req.url);
+    res.render('index', { title:'Home' });
+});
+
+// add blog route 
+router.get('/create', (req, res) => {
+    res.render('add-blog', { title: 'Add Blog' })
+});
+
+
+
+module.exports = router;
